d072950fcd22d9f32281029033a347cd305064eb
Tassiano Alencar <tassiano.alencar@icloud.com>
2016-02-29 18:10:52 -0300

    Modificando namespaces

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,19 +9,20 @@ Route::get('/', 'Front\HomeController@index');
 Route::group(['middleware' => 'web', 'prefix' => 'admin'], function () {
     Route::auth();
 
+    // Home
     Route::get('/', 'HomeController@index');
     Route::get('/home', 'HomeController@index');
 
     // Roles
-    Route::get('/roles', ['as' => 'roles::list', 'uses' => 'RolesController@index']);
-    Route::get('/roles/create', ['as' => 'roles::create', 'uses' => 'RolesController@create']);
-    Route::post('/roles', ['as' => 'roles::store', 'uses' => 'RolesController@store']);
-    Route::get('/roles/{id}', ['as' => 'roles::show', 'uses' => 'RolesController@show']);
-    Route::get('/roles/{id}/edit', ['as' => 'roles::edit', 'uses' => 'RolesController@edit']);
-    Route::put('/roles/{id}', ['as' => 'roles::update', 'uses' => 'RolesController@update']);
-    Route::get('/roles/removePermission/{roleId}/{permissionId}', ['as' => 'roles::removePermission', 'uses' => 'RolesController@removePermission']);
-    Route::post('/roles/addPermission/{roleId}', ['as' => 'roles::addPermission', 'uses' => 'RolesController@addPermission']);
-    Route::get('/roles/{id}/delete', ['as' => 'roles::delete', 'uses' => 'RolesController@destroy']);
+    Route::get('/roles', ['as' => 'roles::list', 'uses' => 'Admin\RolesController@index']);
+    Route::get('/roles/create', ['as' => 'roles::create', 'uses' => 'Admin\RolesController@create']);
+    Route::post('/roles', ['as' => 'roles::store', 'uses' => 'Admin\RolesController@store']);
+    Route::get('/roles/{id}', ['as' => 'roles::show', 'uses' => 'Admin\RolesController@show']);
+    Route::get('/roles/{id}/edit', ['as' => 'roles::edit', 'uses' => 'Admin\RolesController@edit']);
+    Route::put('/roles/{id}', ['as' => 'roles::update', 'uses' => 'Admin\RolesController@update']);
+    Route::get('/roles/removePermission/{roleId}/{permissionId}', ['as' => 'roles::removePermission', 'uses' => 'Admin\RolesController@removePermission']);
+    Route::post('/roles/addPermission/{roleId}', ['as' => 'roles::addPermission', 'uses' => 'Admin\RolesController@addPermission']);
+    Route::get('/roles/{id}/delete', ['as' => 'roles::delete', 'uses' => 'Admin\RolesController@destroy']);
 
     // Users
     Route::get('/users', ['as' => 'users::list', 'uses' => 'Admin\UsersController@index']);
