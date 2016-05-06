af109a5e2594a194819c56d18c0097b6d52e0d88
unknown <mariobrandaobr@hotmail.com>
2016-05-06 18:09:29 -0300

    Packages e gulp.js para automatização.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,32 @@
-var elixir = require('laravel-elixir');
+'use strict';
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+gulp.task('sass', function () {
+	gulp.src('./resources/assets/scss/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./resources/assets/css'));
 });
+
+gulp.task('minify-css', function() {
+	return gulp.src('./resources/assets/css/*.css')
+		.pipe(minifyCss({compatibility: 'ie8'}))
+		.pipe(gulp.dest('./public/assets/css'));
+});
+
+gulp.task('compress', function() {
+	return gulp.src('./resources/assets/js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('./public/assets/js'));
+});
+
+gulp.task('watch', function () {
+	gulp.watch('./resources/assets/scss/*.scss', ['sass']);
+	gulp.watch('./resources/assets/css/*.css', ['minify-css']);
+	gulp.watch('./resources/assets/js/*.js', ['compress']);
+});
+
+gulp.task('default', ['sass', 'minify-css', 'compress', 'watch']);
---
+++ resources/assets/js/script.js
@@ -0,0 +1,5 @@
+/*
+*	Aqui vai seu js
+*	Use o comando gulp para gerar o js.min
+*	Have fun! :)
+*/
